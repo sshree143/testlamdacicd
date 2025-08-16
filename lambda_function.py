0e44b1b8e9351938fca9c4a5688171ca1a3c0416
sasi <s.ram62@gmail.com>
2025-08-16 17:26:55 +0530

    update aws 1

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,12 @@
+# import json
+
+# def lambda_handler(event, context):
+#     # TODO implement
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
+
 import json
 
 def lambda_handler(event, context):
