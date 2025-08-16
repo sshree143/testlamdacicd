b927765cc100a8a76ec51113e4455da755ef82c1
sasi <s.ram62@gmail.com>
2025-08-16 18:33:29 +0530

    update aws 10

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,23 @@
+# import json
+
+# def lambda_handler(event, context):
+#     # TODO implement
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda! from testing team 123 456789')
+#     }
+
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    response_data = {
+        'name': 'John Doe',
+        'age': 30,
+        'phone': '+1-234-567-8901',
+        'email': 'johndoe@example.com'
+    }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! from testing team 123 456789')
+        'body': json.dumps(response_data)
     }
